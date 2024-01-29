1287156c827131b9bfce76494f1bf26784c3996c
Michael Yeung <74708587+moterial@users.noreply.github.com>
2024-01-30 00:43:28 +0800

    erp restaurant

---
+++ restaurant/restaurant/report/stock_ledger/stock_ledger.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Frappe Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Stock Ledger"] = {
+	"filters": [
+
+	]
+};
